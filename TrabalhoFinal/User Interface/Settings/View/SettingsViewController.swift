2c1655c4fa1acdd93a054a18d49c9111b8ed87c0
valmirt <valmirt.dev@gmail.com>
2021-01-29 23:22:51 -0300

    fix

--- TrabalhoFinal/User Interface/Settings/View/SettingsViewController.swift
+++ TrabalhoFinal/User Interface/Settings/View/SettingsViewController.swift
@@ -25,12 +25,11 @@ final class SettingsViewController: UIViewController {
         super.viewDidLoad()
         
         setupView()
-        setupData()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+        setupData()
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
         
